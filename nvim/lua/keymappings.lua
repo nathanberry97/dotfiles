a63d9effdb58fa226828036de35dd52d61167f79
Nathan Berry <nathanberry97@live.com>
2022-10-09 16:52:32 +0100

    adding spell checking

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>b', ':NERDTree .<CR>')
 vim.keymap.set('n', '<leader>f', ':Files .<CR>')
 vim.keymap.set('n', '<leader>n', ':e .<CR>')
+vim.keymap.set('n', '<leader>s', function()
+  vim.o.spell = not vim.o.spell
+end)
