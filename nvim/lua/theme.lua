4d16784dd900b9b21c8fb753c55581836f5beca9
nathan <nathanberry97@live.com>
2023-03-05 22:33:00 +0000

    updates to files

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,6 +1,6 @@
 -- colour scheme
--- vim.opt = 'dark'
--- vim.cmd('autocmd ColorScheme * hi Normal ctermbg=none')
+vim.opt = 'dark'
+vim.cmd('autocmd ColorScheme * hi Normal ctermbg=none')
 vim.cmd('colorscheme gruvbox')
 
 -- setup lualine
