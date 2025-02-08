919d5b5066b0f4a5ab7d1321358a64cdd4102463
nathan <nathanberry97@live.com>
2025-02-08 07:42:22 +0000

    formatting the tabs for a plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,23 +15,23 @@ local plugins = {
 
     -- The following plugin is for note taking
     {
-      'vimwiki/vimwiki',
-      init = function()
-        -- Disable default key mappings
-        vim.g.vimwiki_key_mappings = { all_maps = 0 }
+        'vimwiki/vimwiki',
+        init = function()
+            -- Disable default key mappings
+            vim.g.vimwiki_key_mappings = { all_maps = 0 }
 
-        -- Set Vimwiki settings
-        vim.g.vimwiki_global_ext = 0
-        vim.g.vimwiki_list = {
-          { path = '~/Documents/repos/wiki', syntax = 'markdown', ext = '.md' }
-        }
-      end,
-      config = function()
-        -- Set custom key mappings
-        vim.keymap.set('n', '<enter>', '<Plug>VimwikiFollowLink')
-        vim.keymap.set('n', '<Tab>', '<Plug>VimwikiNextLink')
-        vim.keymap.set('n', '<S-Tab>', '<Plug>VimwikiPrevLink')
-      end
+            -- Set Vimwiki settings
+            vim.g.vimwiki_global_ext = 0
+            vim.g.vimwiki_list = {
+                { path = '~/Documents/repos/wiki', syntax = 'markdown', ext = '.md' }
+            }
+        end,
+        config = function()
+            -- Set custom key mappings
+            vim.keymap.set('n', '<enter>', '<Plug>VimwikiFollowLink')
+            vim.keymap.set('n', '<Tab>', '<Plug>VimwikiNextLink')
+            vim.keymap.set('n', '<S-Tab>', '<Plug>VimwikiPrevLink')
+        end
     },
 
     -- The following plugin is my current theme
