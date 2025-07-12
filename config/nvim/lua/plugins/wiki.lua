4efcbe46e99068870f13054f51406d4453a4276e
Nathan <nathanberry97@live.com>
2025-07-12 20:57:39 +0100

    update vim wiki config

--- config/nvim/lua/plugins/wiki.lua
+++ config/nvim/lua/plugins/wiki.lua
@@ -18,5 +18,43 @@ return {
         vim.keymap.set('n', '<enter>', '<Plug>VimwikiFollowLink')
         vim.keymap.set('n', '<Tab>', '<Plug>VimwikiNextLink')
         vim.keymap.set('n', '<S-Tab>', '<Plug>VimwikiPrevLink')
+
+        -- VimwikiTags function
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "vimwiki",
+            callback = function()
+                vim.api.nvim_buf_create_user_command(0, "VimwikiTags", function()
+                    local handle = io.popen(
+                        "rg '^tags:' ~/Documents/repos/wiki --no-heading | sed 's/^.*tags: //' | tr ' ' '\\n' | sort -u"
+                    )
+                    local result = handle:read("*a")
+                    handle:close()
+
+                    local tags = {}
+                    for tag in result:gmatch("[^\n]+") do
+                        table.insert(tags, tag)
+                    end
+
+                    if vim.fn.empty(tags) == 1 then
+                        print("No tags found")
+                        return
+                    end
+
+                    vim.fn["fzf#run"](vim.fn["fzf#wrap"]({
+                        source = tags,
+                        sink = function(tag)
+                            vim.cmd("Rg " .. tag)
+                        end,
+                        options = "--prompt='Tags> '"
+                    }))
+                end, { desc = "Search Vimwiki tags" })
+
+                -- Only map <Ctrl-t> in Vimwiki buffers
+                vim.keymap.set('n', '<C-t>', ':VimwikiTags<CR>', {
+                    buffer = true,
+                    desc = 'Search Vimwiki tags',
+                })
+            end,
+        })
     end
 }
