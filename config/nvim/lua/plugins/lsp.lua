d7b317f584b9ec933c79b1945254054e04a1616e
Nathan <hey@nathanberry.co.uk>
2026-03-03 21:58:35 +0000

    chore: update to my lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,53 +2,47 @@ return {
     'neovim/nvim-lspconfig',
     version = 'v2.5.0',
     dependencies = {
-        {
-            'williamboman/mason.nvim',
-            version = 'v2.1.0'
-        },
-        {
-            'williamboman/mason-lspconfig.nvim',
-            version = 'v2.1.0'
-        }
+        { 'williamboman/mason.nvim',           version = 'v2.1.0' },
+        { 'williamboman/mason-lspconfig.nvim', version = 'v2.1.0' },
     },
     config = function()
-        -- Define LSPs to install and configure
-        local lsps = {
-            { require('lspconfig').bashls,      'bashls' },
-            { require('lspconfig').cssls,       'cssls' },
-            { require('lspconfig').dockerls,    'dockerls' },
-            { require('lspconfig').gopls,       'gopls' },
-            { require('lspconfig').html,        'html' },
-            { require('lspconfig').lua_ls,      'lua_ls' },
-            { require('lspconfig').pyright,     'pyright' },
-            { require('lspconfig').terraformls, 'terraformls' },
-            { require('lspconfig').tflint,      'tflint' },
-            { require('lspconfig').ts_ls,       'ts_ls' }
-        }
-        local lspToConfigure = {}
-        local lspToInstall = {}
-
-        for i = 1, #lsps do
-            table.insert(lspToConfigure, lsps[i][1])
-            table.insert(lspToInstall, lsps[i][2])
-        end
-
-        -- Configure Mason
         require('mason').setup()
-        require('mason-lspconfig').setup({ ensure_installed = lspToInstall })
 
-        -- Configure LSPs for neovim
+        local servers = {
+            'bashls',
+            'cssls',
+            'dockerls',
+            'gopls',
+            'html',
+            'lua_ls',
+            'pyright',
+            'terraformls',
+            'tflint',
+            'ts_ls'
+        }
+
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        for i = 1, #lspToConfigure do
-            lspToConfigure[i].setup({ capabilities = capabilities })
-        end
 
-        -- Format code on save
-        vim.api.nvim_create_autocmd('BufWritePre', {
-            callback = function() vim.lsp.buf.format({ async = false }) end
+        require('mason-lspconfig').setup({
+            ensure_installed = servers,
+            handlers = {
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+            },
         })
 
-        -- Configure key mappings for LSP
+        local fmt_group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true })
+
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            group = fmt_group,
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end
+        })
+
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
         vim.keymap.set('n', 'ge', vim.diagnostic.open_float, {})
